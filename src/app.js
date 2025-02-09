ddc57cc6f0c6a1a596bace539089cdce98d0cd18
Neverentherthesun <diego_28c@hotmail.com>
2025-02-09 02:13:14 +0000

    cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-import "bootstrap";
 import "./style.css";
+let num = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+let card = ["♦", "♥", "♠", "♣"];
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+function numeros() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let number = Math.floor(Math.random() * num.length);
+
+  return num[number];
+}
+
+function cartas() {
+  //write your code here
+  let car = Math.floor(Math.random() * card.length);
+
+  return card[car];
+}
+// document.getElementById('container')..add(cartas());
+
+let icono = cartas(); //se guarda la funcion una vez y se utiliza el mismo resultado
+
+if (icono == "♦") {
+  document.getElementById("icono-1").style.color = "red";
+  document.getElementById("icono-2").style.color = "red";
+} else if (icono == "♥") {
+  document.getElementById("icono-1").style.color = "red";
+  document.getElementById("icono-2").style.color = "red";
+}
+document.getElementById("number").innerHTML = numeros();
+
+document.getElementById("icono-1").innerHTML = icono;
+document.getElementById("icono-2").innerHTML = icono;
